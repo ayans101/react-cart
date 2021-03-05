6c726bdbb1dea5cc39db3a394bc43697dcd1b5d4
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-05 18:29:38 +0530

    add state in CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,45 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            title: "Book",
+            price: 999,
+            qty: 1,
+            img: ""
+        }
+    }
+
     render() {
+        const { title, price, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} alt=""/>
                 </div>
                 <div className="right-block">
-                    <div style={ { fontSize: 25 } }>Book</div>
-                    <div style={ { color: '#777' } }>Rs 999</div>
-                    <div style={ { color: '#777' } }>Qty: 1</div>
+                    <div style={ { fontSize: 25 } }>{title}</div>
+                    <div style={ { color: '#777' } }>Rs {price}</div>
+                    <div style={ { color: '#777' } }>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1614948672~hmac=50ebb944f7aca6d64047a08d6f4fed4a" 
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1614948735~hmac=04c4e34e2ddc8689fc4acca381e38de9" 
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1614948759~hmac=4e3768dab2daabd7dfd6fea4a71ce7f9" 
+                        />
                     </div>
                 </div>
             </div>
