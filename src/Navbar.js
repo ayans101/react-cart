d215cab31815addf2211b8e8ad1356964ed28e61
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-07 01:53:37 +0530

    add a functional component Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <div style={styles.nav}>
+            <div style={styles.cartIconContainer}>
+                <img style={styles.cartIcon} src="https://www.flaticon.com/svg/vstatic/svg/1170/1170678.svg?token=exp=1615061700~hmac=580f4733a94ec7ae8a92f60b3c4fc580" alt="cart-icon" />
+                <span style={styles.cartCount}>3</span>
+            </div>
+        </div>
+    );
+}
+
+const styles = {
+    cartIcon: {
+        height: 32,
+        marginRight: 20
+    },
+    nav: {
+        height: 70,
+        background: '#64b5f6',
+        display: 'flex',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    cartIconContainer: {
+        position: 'relative'
+    },
+    cartCount: {
+        background: 'yellow',
+        borderRadius: '50%',
+        padding: '4px 8px',
+        position: 'absolute',
+        right: 0,
+        top: -9
+    }
+};
+
+export default Navbar;
