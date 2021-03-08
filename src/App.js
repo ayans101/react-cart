b8fd834a867e2cf2083b3a9f599ae870083e0537
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-09 03:56:00 +0530

    update products in firebase

--- src/App.js
+++ src/App.js
@@ -88,11 +88,25 @@ class App extends React.Component {
     const { products } = this.state;
     const index = products.indexOf(product);
 
-    products[index].qty += 1;
+    // products[index].qty += 1;
 
-    this.setState({
-      products,
-    });
+    // this.setState({
+    //   products,
+    // });
+
+    const docRef = this.db.collection('products').doc(products[index].id);
+
+    docRef
+      .update({
+        qty: products[index].qty + 1
+      })
+      .then(() => {
+        console.log('Updated successfully');
+      })
+      .catch((error) => {
+        console.log('Error : ', error);
+      })
+
   };
   handleDecreaseQuantity = (product) => {
     console.log("Decrease the quantity of ", product);
@@ -103,11 +117,25 @@ class App extends React.Component {
       return;
     }
 
-    products[index].qty -= 1;
+    // products[index].qty -= 1;
 
-    this.setState({
-      products
-    });
+    // this.setState({
+    //   products
+    // });
+
+    const docRef = this.db.collection('products').doc(products[index].id);
+
+    docRef
+      .update({
+        qty: products[index].qty - 1
+      })
+      .then(() => {
+        console.log('Updated successfully');
+      })
+      .catch((error) => {
+        console.log('Error : ', error);
+      })
+
   };
   handleDeleteProduct = (id) => {
     const { products } = this.state;
@@ -165,9 +193,9 @@ class App extends React.Component {
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
-        <button onClick={this.addProduct} style={{ padding: 10, fontSize: 20 }}>
+        {/* <button onClick={this.addProduct} style={{ padding: 10, fontSize: 20 }}>
           Add a product
-        </button>
+        </button> */}
         <Cart 
           products={products}
           onIncreaseQuantity={this.handleIncreaseQuantity}
